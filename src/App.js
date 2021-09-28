adc40c294e636bca8696af109d7f38139fd11438
Rakshith <snrakshith.97@gmail.com>
2021-09-28 12:01:36 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-// import './App.css';
-import axios from "axios";
-// import { useEffect, useState } from "react";
-
 function App() {
   return (
     <div className="App">
-      <h1>Website</h1>
       <p>Welcome to my website</p>
     </div>
   );
