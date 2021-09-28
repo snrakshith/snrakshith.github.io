c3c89a86449daf0d4511f00b1089986ba906af42
Rakshith <snrakshith.97@gmail.com>
2021-09-28 11:34:38 +0530

    Wip for the website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import axios from "axios";
+// import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Website</h1>
+      <p>Welcome to my website</p>
+    </div>
+  );
+}
+
+export default App;
