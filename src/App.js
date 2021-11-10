f6757086bbeadc1e1863658307c61748192a0430
Rakshith <snrakshith.97@gmail.com>
2021-11-10 21:35:18 +0530

    WIP: Removed Files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import Main from "./pages/Main";
-import Footer from "./pages/Footer";
-import About from "./pages/About";
-import Resume from "./pages/Resume";
-
 function App() {
   return (
     <div className="App">
-      <Main />
-      <About />
-      <Resume />
-      <Footer />
+      <h1>Hi</h1>
     </div>
   );
 }
